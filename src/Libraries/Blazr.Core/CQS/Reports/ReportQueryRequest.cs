725aef814d179c5c3ca1fccef9ffc2971db23235
Shaun Curtis <shaun@coldelm.com>
2023-04-02 14:31:27 +0100

    Update

---
+++ src/Libraries/Blazr.Core/CQS/Reports/ReportQueryRequest.cs
@@ -0,0 +1,18 @@
+﻿/// ============================================================
+/// Author: Shaun Curtis, Cold Elm Coders
+/// License: Use And Donate
+/// If you use it, donate something to a charity somewhere
+/// ============================================================
+using System.Linq.Expressions;
+
+namespace Blazr.Core;
+
+public record ReportQueryRequest<TRecord>
+{
+    public int StartIndex { get; init; } = 0;
+    public int PageSize { get; init; } = 1000;
+    public CancellationToken CancellationToken { get; set; } = new();
+    public Expression<Func<TRecord, bool>>? Filter { get; set; }
+    public Expression<Func<TRecord, object>>? Sorter { get; set; }
+    public bool SortDescending { get; init; } = false;
+}
